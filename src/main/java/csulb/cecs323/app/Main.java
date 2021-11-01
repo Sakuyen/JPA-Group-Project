2a3c1db481896298473437db446ed81803666e30
nimasiah <siahnima@gmail.com>
2021-11-01 04:53:36 -0700

    added toString, listInfo, and publishers list

--- src/main/java/csulb/cecs323/app/Main.java
+++ src/main/java/csulb/cecs323/app/Main.java
@@ -23,7 +23,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import java.util.logging.Logger;
-
+//do you see this
 /**
  * A simple application to demonstrate how to persist an object in JPA.
  * <p>
